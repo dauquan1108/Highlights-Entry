ee0fa09f085badaac1c4e23b4e7778685e112d96
quandx <dauquan1108@gmail.com>
2022-06-16 00:20:13 +0700

    first commit

---
+++ src/component/highlightsEntry/index.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2021-present, Bkav, Cop.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Bkav license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @author quandx.bkav@gmail.com on 6/15/2022.
+ *
+ * History:
+ * @modifier abc@bkav.com on xx/xx/xxxx đã chỉnh sửa abcxyx (Chỉ các thay đổi quan trọng mới cần ghi lại note này)
+ */
+
+'use strict';
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function HihlightEntry() {
+    return(
+        <div>quandx</div>
+    );
+}
+
+HihlightEntry.propTypes = {};
+
+HihlightEntry.defaultProps = {};
+
+export default HihlightEntry;
